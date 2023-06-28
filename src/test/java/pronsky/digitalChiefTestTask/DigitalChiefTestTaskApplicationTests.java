c5dbc125db12f0e2fde31bef8fbb636eae49ade9
Andrei Pronsky <andrey.pronsky@gmail.com>
2023-06-28 19:29:40 +0300

    Initial commit

---
+++ src/test/java/pronsky/digitalChiefTestTask/DigitalChiefTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pronsky.digitalChiefTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalChiefTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
