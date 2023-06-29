a1e91ace643290a2843c23f495a2dfbba9002c6d
Andrei Pronsky <andrey.pronsky@gmail.com>
2023-06-29 11:56:27 +0300

    Service methods implemented

--- src/main/java/pronsky/digitalChiefTestTask/service/impl/DirectorServiceImpl.java
+++ src/main/java/pronsky/digitalChiefTestTask/service/impl/DirectorServiceImpl.java
@@ -1,44 +1,69 @@
 package pronsky.digitalChiefTestTask.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pronsky.digitalChiefTestTask.data.entities.Director;
+import pronsky.digitalChiefTestTask.data.repositories.DirectorRepository;
 import pronsky.digitalChiefTestTask.service.DirectorService;
 import pronsky.digitalChiefTestTask.service.dto.DirectorDto;
+import pronsky.digitalChiefTestTask.service.mapper.Mapper;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
+@RequiredArgsConstructor
 public class DirectorServiceImpl implements DirectorService {
+
+    private final Mapper mapper;
+    private final DirectorRepository repository;
 
     @Override
     public DirectorDto getById(Long id) {
-        return null;
+        return repository.findById(id)
+                .map(mapper::toDto)
+                .orElseThrow(() -> new RuntimeException("Unable to find director with id : " + id));
     }
 
     @Override
     public List<DirectorDto> getAll() {
-        return null;
+        return repository.findAll()
+                .stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public DirectorDto getByLastName(String lastName) {
-        return null;
+        return repository.findByLastName(lastName)
+                .map(mapper::toDto)
+                .orElseThrow(() -> new RuntimeException("Unable to find director with lastname : " + lastName));
     }
 
     @Override
     public List<DirectorDto> getByCountryOfBirth(String country) {
-        return null;
+        return repository.findByCountryOfBirth(country)
+                .stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<DirectorDto> getByYearOfBirth(Integer yearOfBirth) {
-        return null;
+        return repository.findAllByYearOfBirth(yearOfBirth)
+                .stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public DirectorDto save(DirectorDto directorDto) {
-        return null;
+        Director director = mapper.toEntity(directorDto);
+        return mapper.toDto(repository.save(director));
     }
 
     @Override
     public void deleteById(Long id) {
-
+        repository.deleteById(id);
     }
 }
--- src/main/java/pronsky/digitalChiefTestTask/service/impl/FilmServiceImpl.java
+++ src/main/java/pronsky/digitalChiefTestTask/service/impl/FilmServiceImpl.java
@@ -1,38 +1,60 @@
 package pronsky.digitalChiefTestTask.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pronsky.digitalChiefTestTask.data.entities.Film;
+import pronsky.digitalChiefTestTask.data.repositories.FilmRepository;
 import pronsky.digitalChiefTestTask.service.FilmService;
 import pronsky.digitalChiefTestTask.service.dto.FilmDto;
+import pronsky.digitalChiefTestTask.service.mapper.Mapper;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
+@RequiredArgsConstructor
 public class FilmServiceImpl implements FilmService {
+
+    private final FilmRepository repository;
+    private final Mapper mapper;
     @Override
     public FilmDto getById(Long id) {
-        return null;
+        return repository.findById(id)
+                .map(mapper::toDto)
+                .orElseThrow(() -> new RuntimeException("Unable to find film with id : " + id));
     }
 
     @Override
     public List<FilmDto> getAll() {
-        return null;
+        return repository.findAll()
+                .stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public FilmDto getByTitle(String title) {
-        return null;
+        return repository.findByTitleContaining(title)
+                .map(mapper::toDto)
+                .orElseThrow(() -> new RuntimeException("Unable to find film containing " + title + "in title"));
     }
 
     @Override
     public List<FilmDto> getByYearOfRelease(Integer year) {
-        return null;
+        return repository.findByYearOfRelease(year)
+                .stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public FilmDto save(FilmDto filmDto) {
-        return null;
+        Film film = mapper.toEntity(filmDto);
+        return mapper.toDto(repository.save(film));
     }
 
     @Override
     public void deleteById(Long id) {
-
+        repository.deleteById(id);
     }
 }
