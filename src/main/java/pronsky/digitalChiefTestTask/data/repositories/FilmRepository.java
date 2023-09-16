c64206365701780632de15d596e6d4c76f15977a
Andrei Pronsky <andrey.pronsky@gmail.com>
2023-09-16 15:20:32 +0300

    refactored

--- src/main/java/pronsky/digitalChiefTestTask/data/repositories/FilmRepository.java
+++ src/main/java/pronsky/digitalChiefTestTask/data/repositories/FilmRepository.java
@@ -3,7 +3,5 @@ package pronsky.digitalChiefTestTask.data.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pronsky.digitalChiefTestTask.data.entities.Film;
 
-import java.util.Optional;
-
 public interface FilmRepository extends JpaRepository<Film, Long> {
 }
